c8714de4e58f1b9c2c48fa51e0bbc551c5bc2e96
ruslansfrov236 <ruslansfrov236@gmail.com>
2025-08-19 21:07:29 +0400

    final

---
+++ core/ecobony.domain/Entities/UserHistory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ecobony.domain.Entities
+{
+    public class UserHistory :BaseEntity
+    {
+        public AppUser User { get; set; }
+
+        public Guid UserId { get; set; }
+        public 
+    }
+}
