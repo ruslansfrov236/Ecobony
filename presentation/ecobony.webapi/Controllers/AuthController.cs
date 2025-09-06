aaea001416c4500ed0055fff847b5a558ab1d88b
ruslansfrov236 <ruslansfrov236@gmail.com>
2025-09-06 22:31:23 +0400

    new controller add auth

---
+++ presentation/ecobony.webapi/Controllers/AuthController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ecobony.webapi.Controllers
+{
+    public class AuthController(IMediator mediator) : Controller
+    {
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginAsyncCommandRequest request)
+        {
+            LoginAsyncCommandResponse response= await mediator.Send(request);
+            return View(response);
+        }
+        [HttpPost("registration")]
+        public async Task<IActionResult> Registration([FromBody] CreateUserCommandRequest request)
+        {
+            CreateUserCommandResponse response = await mediator.Send(request);
+            return View(response);
+        }
+
+        [HttpPost("google-login")]
+        public async Task<IActionResult> Google([FromBody] GoogleLoginAsyncCommandRequest request)
+        {
+            GoogleLoginAsyncCommandResponse response = await mediator.Send(request);
+            return View(response);
+        }
+        [HttpPost("facebook-login")]
+        public async Task<IActionResult> Facebook([FromBody] FacebookLoginAsyncCommandRequest request)
+        {
+            FacebookLoginAsyncCommandResponse response = await mediator.Send(request);
+            return View(response);
+        }
+    }
+}
