2c7f4da078197e6245412f723af4cfa4e0dc8245
ruslansfrov236 <ruslansfrov236@gmail.com>
2025-09-06 11:05:22 +0400

    jwt authosheme redaction

--- infrastructure/ecobony.infrastructure/ServiceRegistration.cs
+++ infrastructure/ecobony.infrastructure/ServiceRegistration.cs
@@ -1,4 +1,4 @@
-
+﻿
 using ecobony.application;
 
 namespace ecobony.infrastructur;
@@ -16,36 +16,47 @@ public static class ServiceRegistration
         {
             options.Configuration = redisConnection;
             options.InstanceName = "SampleInstance";
-           
+
         });
-      
+
         service.AddTransient<ITokenService, TokenService>();
         service.AddTransient<IOTPService, OTPService>();
         service.AddTransient<IMailService, MailService>();
         service.AddTransient<IFileService, FileService>();
-       
+
     }
 
     public static void AddJwtAuthorizeRegistration(this IServiceCollection services, IConfiguration configuration)
     {
-        
-        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-            .AddJwtBearer(options =>
+        var key = Encoding.UTF8.GetBytes(configuration["Token:SecurityKey"]);
+
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+        .AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
             {
-                options.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateAudience = true,
-                    ValidateIssuer = true,
-                    ValidateIssuerSigningKey = true,
-                    ValidAudience = configuration["Token:Audience"],
-                    ValidIssuer = configuration["Token:Issuer"],
-                    IssuerSigningKey =
-                        new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Token:SecurityKey"])),
-                    LifetimeValidator = (notBefore, expires, securityToken, validationParameters) =>
-                        expires != null && expires > DateTime.UtcNow,
-                    NameClaimType = ClaimTypes.Name,
-                    RoleClaimType = ClaimTypes.Role
-                };
-            });
+                ValidateAudience = true,
+                ValidateIssuer = true,
+                ValidateIssuerSigningKey = true,
+                ValidateLifetime = true, // Token müddəti yoxlanacaq
+                ClockSkew = TimeSpan.Zero, // Token vaxt fərqini sıfıra endirir
+                ValidAudience = configuration["Token:Audience"],
+                ValidIssuer = configuration["Token:Issuer"],
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                NameClaimType = ClaimTypes.Name,
+                RoleClaimType = ClaimTypes.Role
+            };
+        });
+
+        services.AddAuthorization(options =>
+        {
+            options.DefaultPolicy = new Microsoft.AspNetCore.Authorization.AuthorizationPolicyBuilder(JwtBearerDefaults.AuthenticationScheme)
+                .RequireAuthenticatedUser()
+                .Build();
+        });
     }
 }
