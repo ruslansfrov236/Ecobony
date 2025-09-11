038c91058ef6b4ec4e10823826eb8063e0eb12db
ruslansfrov236 <ruslansfrov236@gmail.com>
2025-09-11 22:00:18 +0400

    new migration

--- infrastructure/ecobony.persistence/Migrations/20250911172012_mg_6.Designer.cs
+++ infrastructure/ecobony.persistence/Migrations/20250911172012_mg_6.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ecobony.persistence.Context;
 
@@ -11,9 +12,11 @@ using ecobony.persistence.Context;
 namespace ecobony.persistence.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250911172012_mg_6")]
+    partial class mg_6
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -658,6 +661,7 @@ namespace ecobony.persistence.Migrations
                         .HasColumnType("uniqueidentifier");
 
                     b.Property<string>("EntityName")
+                        .IsRequired()
                         .HasColumnType("nvarchar(max)");
 
                     b.Property<DateTime>("UpdateAt")
---
+++ infrastructure/ecobony.persistence/Migrations/20250911172012_mg_6.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ecobony.persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class mg_6 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
