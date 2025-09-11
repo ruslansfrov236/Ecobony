d6f152b0d44948f89f8431acf3863e2d8abde333
ruslansfrov236 <ruslansfrov236@gmail.com>
2025-09-11 22:00:50 +0400

    new Repository class TrashCan

---
+++ infrastructure/ecobony.persistence/Repository/TrashCan/TrashCanReadRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ecobony.persistence.Repository
+{
+    public class TrashCanReadRepository (AppDbContext context): ReadRepository<TrashCan>(context), ITrashCanReadRepository
+    {
+    }
+}
